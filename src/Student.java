02fbc9d7241b8e7f9209ab63f9693437cc309087
xellus <braden.coates@hotmail.com>
2020-01-26 13:48:51 -0500

    Lab 2

--- src/Student.java
+++ src/Student.java
@@ -1,19 +1,59 @@
-public class Student {
-    String fName, lName;
-    int sNum;
+import java.util.regex.Pattern;
 
-    public Student(String fName, String lName, int sNum) {
-        this.fName = fName;
-        this.lName = lName;
-        this.sNum = sNum;
+public class Student {
+    private String fName, lName;
+    private int sNum;
+
+    Student(String fName, String lName, int sNum) {
+        setfName(fName);
+        setlName(lName);
+        setsNum(sNum);
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        if (checkHasNum(fName)) {
+            this.fName = fName;
+        } else {
+            System.out.println("Name must only contain letters");
+        }
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        if (checkHasNum(lName)) {
+            this.lName = lName;
+        } else {
+            System.out.println("Last name must only contain letters");
+        }
+    }
+
+    public int getsNum() {
+        return sNum;
+    }
+
+    public void setsNum(int sNum) {
+        if (String.valueOf(sNum).length() == 7) {
+            this.sNum = sNum;
+        } else {
+            System.out.println("Student number must be 7 characters");
+        }
+    }
+
+    private boolean checkHasNum(String input) {
+        String regex = "[a-zA-Z]+";
+        Pattern pattern = Pattern.compile(regex);
+        return pattern.matcher(input).matches();
     }
 
     @Override
     public String toString() {
-        return "Student{" +
-                "fName='" + fName + '\'' +
-                ", lName='" + lName + '\'' +
-                ", sNum=" + sNum +
-                '}';
+        return fName + " " + lName + " student #: " + sNum;
     }
 }
