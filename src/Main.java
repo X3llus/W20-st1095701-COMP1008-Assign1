1a7cbcea5036e2de35291c7de720969776dbe185
xellus <braden.coates@hotmail.com>
2020-02-03 18:51:48 -0500

    Lab 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
         Student studentOne = new Student("Braden", "Coates", 1095701);
         System.out.println(studentOne.toString());
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        stage.setTitle("Student Card");
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
